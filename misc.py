8bfd838e04542dca03fe3b5860bf425a59c21103
Forumbit <58177045+Forumbit@users.noreply.github.com>
2021-01-31 16:53:01 +0300

    Update misc.py

--- misc.py
+++ misc.py
@@ -1,7 +1,8 @@
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import logging
 from aiogram import Bot, Dispatcher
+import os
 
-bot = Bot(token="1547077005:AAEzkOMfPdEU5On_0Jfr3kXNJy9kWm_BqAg", parse_mode="html")
+bot = Bot(token="os.environ.get('BOT_TOKEN')", parse_mode="html")
 dp = Dispatcher(bot, storage=MemoryStorage())
 logging.basicConfig(level=logging.INFO)

b355c7d1fdf1c5feaac48a25ec30902a2d33b4e1
Forumbit <58177045+Forumbit@users.noreply.github.com>
2021-07-31 18:52:10 +0300

    Delete the mirror server

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 from misc import dp
 from aiogram import executor
 import handlers
-from aiogram.bot import api
-PATHCED_URL = "https://telegg.ru/orig/bot{token}/{method}"
-setattr(api, "API_URL", PATHCED_URL)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates = True)
